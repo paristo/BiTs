93b4851bb1c50518f7546d29cd4d3dc31f403133
bujifuji <33360542+bujifuji@users.noreply.github.com>
2018-02-02 22:45:52 -0500

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,1 +1,30 @@
-penus 
+"""
+This is the GUI file for BiTs
+"""
+from tkinter import *
+master = Tk()
+f = Frame(master, height=5, width=5)
+f.pack_propagate(0) # don't shrink
+f.pack()
+
+
+class pixel:
+    def __init__(self, r, c):
+        self.row = r
+        self.column = c
+        p = Button(f, relief="flat", bg="white")
+        p.grid(row = self.row, column = self.column)
+   # def  filler(self, ):
+
+def startup(c):
+    for i in range(2):
+        c.append(i)
+        for j in range(2):
+            c[i] = pixel(i,j)
+        
+        
+        
+
+while True:
+    canvas = []
+    startup(canvas)
