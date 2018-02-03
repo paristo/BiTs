b4e63b4ea614c3ae04fec49bb72f9f21f467d676
paristo <33398707+paristo@users.noreply.github.com>
2018-02-03 12:39:50 -0500

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -2,10 +2,12 @@
 This is the GUI file for BiTs
 """
 from Tkinter import *
+import time
 from tkColorChooser import askcolor
 master = Tk()
 RC = 200
 BOX = 1000
+LOCK = False
 
 
 class Pixel:
@@ -15,9 +17,10 @@ class Pixel:
         self.board = e.create_rectangle(c*width, r*height, (c+1)*width, (r+1)*height, fill="white", outline="")
 
     def filler(self):
+        global LOCK
         color = askcolor()
-        color = color[-7:]
         e.itemconfigure(self.board, fill=color[1])
+        LOCK = True
 
 
 def startup():
@@ -27,15 +30,22 @@ def startup():
 
 
 def eyedrop(event):
+    global LOCK
+    if LOCK:
+        time.sleep(5)
+        LOCK = False
     j = event.x//width
     i = event.y//height
     pxl[i][j].filler()
 
+
 e = Canvas(master, width=BOX, height=BOX, bg="white")
 e.bind("<Button-1>", eyedrop)
 e.pack()
+
 width = BOX / RC
 height = BOX / RC
 pxl = [[None for _ in range(RC)]for _ in range(RC)]
+
 startup()
 mainloop()
